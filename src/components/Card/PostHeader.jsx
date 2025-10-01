d8802b7588dfdd538d1f9c3b009482ff1c40e95e
emanawad14 <emanelnady2001@gmail.com>
2025-10-01 20:31:31 +0300

    finished

--- src/components/Card/PostHeader.jsx
+++ src/components/Card/PostHeader.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import image from '../../../public/download (1).png'
 
 export default function PostHeader({photo, name,data}) {
   return (
     <div className="flex items-center gap-3 w-full ">
       
               <img
-              onError={(e)=>e.target.src='../../../public/download (1).png'}
+              onError={(e) => { e.target.src = image }}
+
                 className="rounded-full w-10 h-10 md:w-12 md:h-12 object-cover"
                 src={photo}
                 alt={name}
